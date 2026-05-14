ad37415b463b49684d8b7b2b61e19b2268b194e2
Third-img <pascualquerubin06@gmail.com>
2026-05-14 17:02:14 +0800

    initialize colors and font

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,3 +1,36 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
 export default function Timer() {
-  return <>Hello, World</>;
+  return (
+    <div
+      className="
+      font-bebas 
+      text-xl
+      text-crimson-carrot
+      flex 
+      flex-col
+      items-center
+      justify-center
+      min-h-svh
+      w-full
+      "
+    >
+      <div
+        className="
+      text-5xl
+      tracking-tight
+      "
+      >
+        Sprint One
+      </div>
+      <div
+        className="
+      text-9xl
+      "
+      >
+        00:00
+      </div>
+    </div>
+  );
 }
